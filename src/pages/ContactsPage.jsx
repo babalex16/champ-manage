94c8878833cb30ce8b941b6cc85a1a1b7886e7fe
babalex16 <sasha.babii@mail.ru>
2023-09-24 19:00:28 +0200

    Contacts and Rules pages done

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+import React from 'react';
+import coverImg from '../assets/photos/mjjf-fb-cover.png';
+import Footer from '../components/Footer/Footer';
+import Image from 'react-bootstrap/Image';
+import { Container, Table } from 'react-bootstrap';
 
 function ContactsPage() {
   return (
-    <div style={{ paddingTop: '80px', backgroundColor: '#5c8374', minHeight: '100vh' }}>
-      ContactPage
+    <div style={{ paddingTop: '75px', backgroundColor: '#5c8374', minHeight: '100vh' }}>
+      <Image src={coverImg} fluid />;
+      <h2 className="text-white text-center mt-4">Contacts</h2>
+      <h5 className='text-white my-3'>  </h5>
+      <Container className="mt-5">
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>Coach</th>
+              <th>Address</th>
+              <th>Schedule</th>
+              <th>Phone Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Roman Babii</td>
+              <td>Bulevardul Decebal 2, Chisinau</td>
+              <td>
+                  <p>Monday, Wednesday, Friday</p>
+                <ul>
+                  <li>16:00-17:00 / 6-9 years</li>
+                  <li>17:00-18:15 / 10-12 years</li>
+                  <li>18:30-19:45 / 12-15 years</li>
+                  <li>20:00-21:30 / 16+ years</li>
+                </ul>
+              </td>
+              <td>
+                +373 688 40 540
+              </td>
+            </tr>
+            <tr>
+              <td>Artem Kitaev</td>
+              <td>
+                str.Alba-Iulia 113, Sport House, Chisinau
+              </td>
+              <td>
+                <ul>
+                  <li>Monday-Friday: 08:00-22:00</li>
+                  <li>Saturday: 08:00-20:00</li>
+                </ul>
+              </td>
+              <td>
+                +373 681 32 424
+              </td>
+            </tr>
+            <tr>
+              <td>Vlad Trifanov</td>
+              <td>
+                str. Eugen Doga 5, Chisinau
+              </td>
+              <td>
+                <ul>
+                  <li>Tuesday, Thursday: 17:00-21:30</li>
+                  <li>Saturday: 10:00-13:30</li>
+                </ul>
+              </td>
+              <td>
+                +373 680 55 447
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default ContactsPage
+export default ContactsPage;
--- src/pages/RulesPage.jsx
+++ src/pages/RulesPage.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import ruleBook from '../assets/pdfs/EN_IBJJF_RulesBook_MAR2022.pdf'
+import ruleImg from '../assets/photos/rule-book.jpg';
+import Footer from '../components/Footer/Footer';
 
 function RulesPage() {
   return (
     <div style={{ paddingTop: '80px', backgroundColor: '#5c8374', minHeight: '100vh' }}>
-      RulesPage
+      <h2 className="text-white text-center mt-4">Rules</h2>
+      <Image src={ruleImg} fluid className="mx-auto d-block" />
+      <p className="text-center text-white">
+        RULE BOOK
+        <br />
+        Click on the button below to download the latest IBJJF Rule Book (v5.2)
+      </p>
+      <Button href={ruleBook} download variant="primary" size="lg" className="mx-auto d-block mt-3" style={{width: '200px'}}>Download here</Button>
+      <Footer/>
     </div>
-  )
+  );
+  
 }
 
 export default RulesPage
